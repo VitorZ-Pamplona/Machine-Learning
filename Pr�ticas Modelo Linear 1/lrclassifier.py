c5f9aa3978f8f6fadb753afc13c589c4b00c022f
Vitor Zimbrunes Pamplona <111309532+VitorZ-Pamplona@users.noreply.github.com>
2023-07-07 20:25:05 -0300

    Add files via upload

---
+++ Pr�ticas Modelo Linear 1/lrclassifier.py
@@ -0,0 +1,11 @@
+from linearregression import LinearRegression
+import numpy as np
+
+
+class LRClassifier():
+    def execute(self, _X, _y):
+                
+    def predict(self, x_):
+     
+    def getRegressionY(self, regressionX, shift=0):
+        return (-self.w[0]+shift - self.w[1]*regressionX) / self.w[2]
